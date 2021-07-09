0ae0ec345d489fb81755caee9ab91fb8955f6cfa
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-10 08:05:23 +0900

    #69 Finished Implemtetation

--- spec/features/staff/phone_management_spec.rb
+++ spec/features/staff/phone_management_spec.rb
@@ -25,11 +25,30 @@ feature '職員による顧客電話番号管理' do
     click_link '顧客管理'
     first('table.listing').click_link '編集'
     fill_in 'form_home_address_phones_0_number', with: '03-9999-9999'
+    fill_in 'form_home_address_phones_1_number', with: '03-4567-8987'
     check 'form_home_address_phones_0_primary'
     click_button '更新'
 
     customer.reload
-    expect(customer.home_address.phones.size).to eq(1)
+    expect(customer.home_address.phones.size).to eq(2)
     expect(customer.home_address.phones[0].number).to eq('03-9999-9999')
+    expect(customer.home_address.phones[0].primary).to be_truthy
+    expect(customer.home_address.phones[1].number).to eq('03-4567-8987')
+    expect(customer.home_address.phones[1].primary).to be_falsey
+  end
+  scenario '職員が顧客の会社の電話番号を追加する' do
+    click_link '顧客管理'
+    first('table.listing').click_link '編集'
+    fill_in 'form_work_address_phones_0_number', with: '03-9999-9999'
+    fill_in 'form_work_address_phones_1_number', with: '03-4567-8987'
+    check 'form_work_address_phones_0_primary'
+    click_button '更新'
+
+    customer.reload
+    expect(customer.work_address.phones.size).to eq(2)
+    expect(customer.work_address.phones[0].number).to eq('03-9999-9999')
+    expect(customer.work_address.phones[0].primary).to be_truthy
+    expect(customer.work_address.phones[1].number).to eq('03-4567-8987')
+    expect(customer.work_address.phones[1].primary).to be_falsey
   end
 end
