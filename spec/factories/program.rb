349ce2f41ce071a3933a2f85ff70b1f5c78d9751
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-10 15:40:59 +0900

    #72 Add Rspec

--- spec/factories/program.rb
+++ spec/factories/program.rb
@@ -3,17 +3,9 @@ FactoryBot.define do
     title {"test"}
     description {"test"}
     application_start_time {Time.new(2021,7,5,12,15)}
-    application_start_time {Time.new(2021,7,12,12,15)}
+    application_end_time {Time.new(2021,7,12,12,15)}
 
-    password              { '12345678' }
-    password_confirmation { '12345678' }
-    type { 'Staff' }
-    family_name { '山田' }
-    given_name { '太郎' }
-    family_name_kana { 'ヤマダ' }
-    given_name_kana { 'タロウ' }
-    start_date { Date.yesterday }
-    end_date { nil }
-    suspended { false }
+    min_number_of_participants {'3'}
+    max_number_of_participants {'6'}
   end
 end
